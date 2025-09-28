bfccc766897fca20bbac913f8e97172f1c184575
dukeph0 <dpham5161@outlook.com>
2025-09-28 17:06:46 -0600

    Add Module5_Activity

---
+++ Module5_Activity.py
@@ -0,0 +1,14 @@
+# Purpose: Develop a Python program that uses a semaphore to manage process synchronization and prevent deadlocks.
+# This includes: 
+# - Being able to retrieve semaphore initialization details
+# - Enforcing the maximum output of concurrent processes
+# - Implementing a structured output pattern
+
+# Expected Result: Displays the use of a semaphore to manage process synchronization and prevent deadlocks
+
+# References: 
+
+# Version   Author      Date            Description
+# 1         Duke Pham   2025-09-28      :D
+
+import threading 
